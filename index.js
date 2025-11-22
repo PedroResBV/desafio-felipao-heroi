0a6b607d6453097d7343cf19387c6aa741a72156
PedroResBv <pedrohresende.santos@gmail.com>
2025-11-22 09:52:28 -0300

    Projeto Concluído

---
+++ index.js
@@ -0,0 +1,46 @@
+
+class heroi{
+	constructor(nomeHeroi, idadeHeroi, tipoHeroi){
+    	this.nomeHeroi = nomeHeroi
+        this.idadeHeroi= idadeHeroi
+        this.tipoHeroi = tipoHeroi
+    }
+    
+    atacar(){
+        
+        let ataque 
+        switch(this.tipoHeroi){
+            
+            case "mago":
+                ataque = "magia"
+                break
+            case "guerreiro":
+                ataque =  "espada"
+                break
+            case "monge":
+                ataque = "artes marciais"
+                break
+            case "ninja":
+                ataque = "shuriken"
+                break
+            default:
+                break
+    	}
+        console.log(`O ${this.tipoHeroi} atacou usando ${ataque}`)
+        
+    } 
+    
+    
+} 
+
+
+
+let ninjaPedro = new heroi("Pedro", "29 anos", "ninja")
+
+let magaJessica = new heroi("Jessica", "27 anos", "mago")
+
+
+
+ninjaPedro.atacar()
+magaJessica.atacar()
+
